0668d642c83400e65a5373ec4d77a4fb00dfe95d
Wendy Jiang <wjiang.student@hsfi.us>
2017-02-01 21:42:59 +0000

    added square

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 // JavaScript File
 TinyTurtle.apply(window); 
 
-
-right(150);
 forward(50);
-right(250);
-forward(25);
+right(90);
+forward(50);
+right(90);
+forward(50);
+right(90);
+forward(50)
 
-
-
-stamp();
